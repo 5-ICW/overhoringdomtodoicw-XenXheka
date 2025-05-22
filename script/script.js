93ec22203ba0b5149b35b396a1ff252fe9926503
XenXheka <xen.xheka@student.tsaam.be>
2025-05-22 12:15:46 +0200

    Laatste versie

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@ const input = document.getElementById("todo-input");
 const list = document.getElementById("todo-list");
 const selectWinkel = document.getElementById("selectWinkel");
 const taken = [
+  // lijst voor de taken
   {
     id: 1,
     taak: "Studeren",
@@ -18,6 +19,7 @@ const taken = [
 ];
 
 const winkels = [
+  //lijst voor de winkels
   {
     id: 1,
     winkel: "Delhaize",
@@ -33,12 +35,15 @@ const winkels = [
 ];
 let lijst = [];
 winkels.forEach((winkel) => {
+  // opties maken van de winkels
   const winkelOptions = document.createElement("option");
   winkelOptions.innerText = winkel.winkel;
   winkelOptions.value = winkel.winkel;
   selectWinkel.appendChild(winkelOptions);
 });
 function winkelSelecteeren() {
+  //Selecteren van winkel en bij toevoegen van taak de winkel zien
+  // je zal de winkel pas kunnen zien als je eerst de winkel aldi of carfour neemt.
   selectWinkel.addEventListener("change", function (e) {
     const target = e.target;
     lijst.push(target.value);
@@ -63,6 +68,7 @@ form.addEventListener("submit", function (e) {
 });
 
 taken.forEach((taak) => {
+  // tonen van taken die vooraf zijn gemaakt.
   const taakLi = document.createElement("li");
   taakLi.innerText = taak.taak;
   taakLi.value = taak.taak;
